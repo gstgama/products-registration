3da114b7fcd1eaab6d1bbd662840498f94d8e785
Gustavo Gama Cardozo <gustavogamacardozo@gmail.com>
2022-04-07 07:53:03 -0300

    initial commit

---
+++ back/src/API/Controllers/ProductController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class ProductController : ControllerBase
+  {
+    private readonly DataContext _context;
+    public ProductController(DataContext context)
+    {
+      _context = context;
+    }
+
+    public IEnumerable<Product> product = new Product[] {
+        new Product(){
+        Id = 1,
+        Name = "Bateria",
+        Category = "Office",
+        Price = 33.12
+      },
+        new Product(){
+          Id = 2,
+          Name = "Bateria 2",
+          Category = "Office 2",
+          Price = 66.12
+        }
+      };
+
+    [HttpGet]
+    public IEnumerable<Product> GetAll()
+    {
+      return _context.Products;
+    }
+
+    [HttpGet("{id}")]
+    public Product GetById(int id)
+    {
+      return _context.Products.FirstOrDefault(data => data.Id == id);
+    }
+
+  }
+}
